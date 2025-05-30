115bff44c0fb5da9e2cd94bca029b777bcf5eaf1
dcr_dujqb <dcr_dujqb@dcits.com>
2025-05-30 16:36:43 +0800

    feat: update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
+import { getAllNotes } from "@/lib/redis";
+import SidebarNoteList from "@/components/SidebarNoteList";
 
 export default async function Sidebar() {
+  const notes = await getAllNotes();
   return (
     <>
       <section className="col sidebar">
@@ -22,7 +25,9 @@ export default async function Sidebar() {
         <section className="sidebar-menu" role="menubar">
           {/* SideSearchField */}
         </section>
-        <nav>{/* SidebarNoteList */}</nav>
+        <nav>
+          <SidebarNoteList notes={notes} />
+        </nav>
       </section>
     </>
   );
